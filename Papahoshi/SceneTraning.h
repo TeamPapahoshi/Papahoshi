37cba5464987bb6780d0035304d4732310d11d5c
MeiMeiMeiMeiMeiMeiMeiMei <hal.mei1223@gmail.com>
2018-02-28 03:19:24 +0900

    トレモ追加。 レバー回転でトレモに行く

---
+++ Papahoshi/SceneTraning.h
@@ -0,0 +1,65 @@
+//===========================================
+//
+// Papahoshi  SceneTraning.h
+//
+// Mei Goto.
+//
+//===========================================
+
+#ifndef __SCENE_TRANING_H__
+#define __SCENE_TRANING_H__
+
+//-----------------------------
+// インクルード部
+//-----------------------------
+#include "SceneManeger.h"
+#include "Common.h"
+#include "PlaySupport.h"
+#include "StageManager.h"
+#include "BG.h"
+#include "Net_Type2.h"
+#include "SampleStar.h"
+#include "NormalStars.h"
+#include "FixedStars.h"
+#include "Ryusei.h"
+#include "Constellation.h"
+#include "Gage.h"
+#include "BlackHole.h"
+#include "SpaceRock.h"
+#include "GameUI.h"
+#include "Announce.h"
+#include "Timer.h"
+
+//-----------------------------
+// クラス定義
+//-----------------------------
+class cSceneTraning : public cBaseScene{
+
+public:
+	cSceneTraning();
+	~cSceneTraning();
+	void Update();
+	void Draw();
+
+private:
+	cBG*						m_pBG;
+	cNet*						m_pNet;
+	cNormalStar*				m_pNomalStar;		// モブ星
+	cBlackHole*					m_pBlackHole;		// ブラックホール
+	cSpaceRock*					m_pSpaceRock;		// 隕石
+	vector<cFixedStar*>			m_pFixedStar;		// 恒星
+	cRyusei*					m_pRyusei;			// 流星
+	cConstellation*				m_pConsellation;	// 星群
+	cGage*						m_pGage;			// ゲージ
+	cGameUI*					m_pGameUI;
+	cTimer*						m_pTimer;			// タイマー
+	cSampleStar*				m_pSampleStar;
+	cAnnounce*					m_pAnnounce;
+	cPlaySupport*				m_pPlaySupport;
+
+	bool m_bFever;				// フィーバタイムかどうか
+
+	void CheckCollision();	//当たり判定を取る
+};
+
+#endif
